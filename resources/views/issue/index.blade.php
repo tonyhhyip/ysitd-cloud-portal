814cc0c6eaeeb3997d63de7449eab8db8ce9f1f6
Tony Yip <tony@opensource.hk>
2016-08-17 01:12:21 +0800

    Change No issue display behave

--- resources/views/issue/index.blade.php
+++ resources/views/issue/index.blade.php
@@ -6,19 +6,23 @@
             <a class="btn" href="{{ route('issue.create') }}">Report New Issue</a>
         </div>
         <div class="col s12 m9 l10">
-            <div class="collection">
-                @forelse($issues as $issue)
-                    <a class="collection-item waves-effect" href="{{ route('issue.show', ['issue' => $issue->id]) }}">
-                        <span class="text-gray">#{{$issue->id}}</span>
-                        {{ $issue->title }}
-                    </a>
-                @empty
-                    <div class="card-inner">
-                        No issue is open.
+            @if(count($issues))
+                <div class="collection">
+                    @forelse($issues as $issue)
+                        <a class="collection-item waves-effect" href="{{ route('issue.show', ['issue' => $issue->id]) }}">
+                            <span class="text-gray">#{{$issue->id}}</span>
+                            {{ $issue->title }}
+                        </a>
+                    @endforeach
+                </div>
+                {!! $issues->render() !!}
+            @else
+                <div class="card small">
+                    <div class="card-content">
+                        <span class="card-title">No issue is open.</span>
                     </div>
-                @endforelse
-            </div>
-            {!! $issues->render() !!}
+                </div>
+            @endif
         </div>
     </div>
 @endsection
