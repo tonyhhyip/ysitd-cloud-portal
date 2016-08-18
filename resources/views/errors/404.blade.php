acf915156dcb7b4c7b0d24bd7443c34b92f0d0f5
Tony Yip <tony@opensource.hk>
2016-08-18 10:27:26 +0800

    Apply error layout to error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,20 +1,9 @@
-@extends('layouts/default/template')
+@extends('layouts/default/error')
 @section('title', '404 Not Found')
 @section('content')
-    <div class="row">
-        <div class="col s24 m12">
-            <div class="card z-depth-5">
-                <div class="card-content">
-                    <div class="card-title">404 Not Found</div>
-                    <div>
-                    <p class="text-strong">
-                        You are <strong class="text-upper">korued</strong>!<br>
-                        The url or resource you are looking for is not found!<br>
-                        Please go back or go to home page to continue.
-                    </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <p>
+        You are <strong class="text-upper">korued</strong>!<br>
+        The url or resource you are looking for is not found!
+        Please go back or go to home page to continue.
+    </p>
 @endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,18 +1,9 @@
-@extends('layouts/default/template')
+@extends('layouts/default/error')
 @section('title', '500 Korued')
 @section('content')
-    <div class="row">
-        <div class="col s24 m12">
-            <div class="card z-depth-5">
-                <div class="card-content">
-                    <div class="card-title">500 Korued</div>
-                    <p class="text-strong">
-                        You are <strong class="text-upper">korued</strong>!<br>
-                        Something go wrong!<br>
-                        Please report to us and check if <a href="https://telegram.me/koru1130">@koru1130</a> is nearby.
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
+    <p>
+            You are <strong class="text-upper">korued</strong>!<br>
+    Something go wrong!<br>
+    Please report to us and check if <a href="https://telegram.me/koru1130">@koru1130</a> is nearby.
+    </p>
 @endsection
