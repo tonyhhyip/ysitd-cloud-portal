e6fb9c75d17bfa696615fdc871f1ffa9a1d1ae8a
Tony Yip <tony@opensource.hk>
2016-09-02 20:59:24 +0800

    Create basic dashboard

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts/document/template')
+
+@section('title', 'Admin Dashboard')
+
+@section('content')
+    <div class="row">
+        <div class="col m6 s12">
+            <hit-rate></hit-rate>
+        </div>
+    </div>
+@endsection
