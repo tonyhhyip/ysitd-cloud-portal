1100bb2752e247ffa20435cc7833f6a1c54b369b
Tony Yip <tony@opensource.hk>
2016-09-02 21:00:45 +0800

    Create avatar and user icon component

---
+++ resources/assets/js/components/Avatar.vue
@@ -0,0 +1,31 @@
+<template>
+    <span :class="[classes, class]">
+        <img :src="src">
+    </span>
+</template>
+<script>
+    export default{
+        data() {
+          return {
+              classes: {
+                  'avatar': true,
+                  'avatar-sm': this.size === 'medium',
+                  'avatar-lg': this.size === 'large',
+                  'avatar-xs': this.size === 'small'
+              }
+          }
+        },
+        props: {
+            src: {
+                type: String,
+                required: true
+            },
+            size: {
+                type: String
+            },
+            class: {
+                type: Object
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/components/UserIcon.vue
@@ -0,0 +1,29 @@
+<template>
+    <avatar :src="src" :size="size" :style="{ display: display }"></avatar>
+</template>
+
+<script>
+    export default{
+        data() {
+            return {
+                src: `/user/${this.user}/icon`,
+                display: this.type === 'inline' ? 'inline-block' : 'block'
+            }
+        },
+        props: {
+            user: {
+                type: String,
+                required: true
+            },
+            size: {
+                type: String
+            },
+            type: {
+                type: String
+            }
+        },
+        components:{
+            'avatar': require('./Avatar.vue')
+        }
+    }
+</script>
