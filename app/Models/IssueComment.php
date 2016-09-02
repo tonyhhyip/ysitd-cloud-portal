49ca9799c2c51fbf0035fe53157cf726daf53ae5
Tony Yip <tony@opensource.hk>
2016-09-02 20:45:59 +0800

    Add Issue Comment  Model attribute

--- app/Models/IssueComment.php
+++ app/Models/IssueComment.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class IssueComment extends Model
 {
+    public $incrementing = false;
 
+    public function comment()
+    {
+        return $this->belongsTo(Issue::class);
+    }
 }
