70474f3f8199421bb48af19ebdd760d4bbbbec4e
Tony Yip <tony@opensource.hk>
2016-09-02 20:54:22 +0800

    Merge script to init
    - Set init to be starting point script
    - remove ajax script and replace with vue resource

--- resources/assets/js/collapse.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const route = location.pathname.replace(/^\//, '').split('/')[0];
-const ele = document.getElementById(`collapse-${route}`);
-if (ele && ele !== null) {
-  $(ele).addClass('active');
-}
--- resources/assets/js/init.js
+++ resources/assets/js/init.js
@@ -1,3 +1,14 @@
 'use strict';
+import Vue from 'vue';
+import VueMaterialComponent from 'vue-material-components';
+Vue.use(VueMaterialComponent);
+
+const route = location.pathname.replace(/^\//, '').split('/')[0];
+const ele = document.getElementById(`collapse-${route}`);
+if (ele && ele !== null) {
+  $(ele).addClass('active');
+  $('.collapsible-header', ele).addClass('active');
+}
+
 $(document.getElementsByClassName('button-collapse')).sideNav();
 $(document.getElementsByClassName('dropdown-button')).dropdown();
